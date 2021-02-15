127db843735d83e69ffa90c4d8028e32a7720332
R1314 <51386679+R1314@users.noreply.github.com>
2021-02-15 17:45:50 -0300

    Update addroute.sh

--- addroute.sh
+++ addroute.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #add route brute force
 
-for objetivo in $(seq 1 256);do echo $objetivo; ip route ad 10.16.34.$objetivo via $1;done
+for objetivo in $(seq 1 256);do echo $objetivo; ip route add 10.16.34.$objetivo via $1;done
